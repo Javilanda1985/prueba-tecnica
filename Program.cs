b2d6ef8760edd8cabc87cbc82c2ad61e53132d26
Javier L <javierlanda1985@gmail.com>
2022-09-30 14:05:59 -0500

    fibonacci

--- Program.cs
+++ Program.cs
@@ -1,10 +1,55 @@
 ﻿
+using System;
+
 namespace Desafio
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            fibonacci();
+            Console.ReadKey();
+
         }
+
+        static void fibonacci()
+        {
+            int[] prueba;
+            int numero, a = 0, b = 1, c;
+            Console.WriteLine("**********************************");
+            Console.WriteLine("\n ---- Sucesion de Fibonacci ----");
+            Console.Write("\nCantidad de numeros a ingresar: ");
+            numero = int.Parse(Console.ReadLine());
+
+            prueba = new int[numero];
+            bool fibo = false;
+
+            Console.WriteLine("");
+            for (int i = 0; i < numero; i++)
+            {
+                Console.Write("Ingrese # " + (i + 1) + ": ");
+                prueba[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numero; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                if (prueba[i] == a)
+                {
+                    fibo = true;
+                }
+                else
+                {
+                    fibo = false;
+                    break;
+                }
+            }
+            Console.WriteLine("\nLa sucesion de fibonacci  ingresada es: " + fibo);
+            Console.WriteLine("");
+        }
+
+
     }
 }
